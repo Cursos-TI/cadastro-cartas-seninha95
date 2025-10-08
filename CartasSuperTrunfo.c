beea6b951c8885de56dcaa8d8ec65b39f54e71e8
seninha95 <senna.matheus18@gmail.com>
2025-10-08 00:41:55 +0000

    criando o codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,16 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    char Estado = 'A';
+    char Código[20] = "A01";
+    char Cidade[20] = "São paulo";
+    int População = 12325000;
+    float Área = 1521.11;
+    float Pib = 699.28;
+    int Número de Pontos Turísticos = 50;
 
+  // Área para definição das variáveis para armazenar as propriedades das cidades
+  printf("O Código é")
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
